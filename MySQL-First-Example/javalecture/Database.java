178630e0a088d2857538a50e86b818c64dc8b9fb
George Khananaev <george.khananaev@gmail.com>
2022-12-29 16:31:10 +0200

    polymorphism and inheritance

---
+++ MySQL-First-Example/javalecture/Database.java
@@ -0,0 +1,30 @@
+package com.javalecture;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public class Database {
+
+    public static void execute() throws ClassNotFoundException {
+
+        try(Connection con = DriverManager.getConnection("jdbc:mysql://192.168.1.159:3306/books","root","139745698");
+            Statement stmt = con.createStatement())
+        {
+
+            String query = "SELECT title FROM Book";
+
+            ResultSet rs = stmt.executeQuery(query);
+
+            ArrayList<String> arr = new ArrayList<>();
+
+            while (rs.next()) {
+                arr.add(rs.getString("title"));
+            }
+
+            System.out.println(arr);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ MySQL-First-Example/javalecture/Main.java
@@ -0,0 +1,9 @@
+package com.javalecture;
+
+public class Main {
+
+    public static void main(String[] args) throws ClassNotFoundException {
+        Database.execute();
+    }
+
+}
